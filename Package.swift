13e785c1e99a4b8ad8de2492dcdb4a00b864cdaa
Brian Chon <chon@adobe.com>
2018-08-19 04:02:31 -0400

    fix

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ let package = Package(
     name: "InstaServer",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", .exact("3.0.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.0.0")),
         
-        .package(url: "https://github.com/vapor/auth.git", .upToNextMajor(from: "2.0.0")),
+//        .package(url: "https://github.com/vapor/auth.git", .upToNextMajor(from: "2.0.0")),
         
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from: "3.0.0")),
@@ -18,7 +18,7 @@ let package = Package(
         
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Routing", "Multipart", "Authentication"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Routing", "Multipart"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"]),
     ]
