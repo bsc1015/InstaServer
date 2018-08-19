062f355d95540889720f25718f73fc9492b568e9
Brian Chon <chon@adobe.com>
2018-08-19 03:51:23 -0400

    update package

--- Package.swift
+++ Package.swift
@@ -5,16 +5,16 @@ let package = Package(
     name: "InstaServer",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.0.8")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "3.0.0")),
         
-        .package(url: "https://github.com/vapor/auth.git", .upToNextMajor(from: "2.0.1")),
+        .package(url: "https://github.com/vapor/auth.git", .upToNextMajor(from: "2.0.0")),
         
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMajor(from: "3.0.0")),
         
-        .package(url: "https://github.com/vapor/routing.git", .upToNextMajor(from: "3.0.1")),
+        .package(url: "https://github.com/vapor/routing.git", .upToNextMajor(from: "3.0.0")),
         
-        .package(url: "https://github.com/vapor/multipart.git", .upToNextMajor(from: "3.0.2")),
+        .package(url: "https://github.com/vapor/multipart.git", .upToNextMajor(from: "3.0.0")),
         
     ],
     targets: [
